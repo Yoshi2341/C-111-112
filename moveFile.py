f5ba8750a5169621b64fd80243441e94c4b82e07
WHITE444 <87076547+Yoshi2341@users.noreply.github.com>
2022-10-03 18:12:11 +0530

    Add files via upload

---
+++ moveFile.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+from_dir = "C:\Users\lenovo\Downloads\downloadimages"
+to_dir = "C:\Users\lenovo\OneDrive\Desktop\ile-white"
+
+list_of_files = os.listdir(from_dir)
+#print(list_of_files)
+
+for file_name in list_of_files:
+    name,extension=os.path.splitext(file_name)
+print(name)
+print(extension)
+
+if extension == '':
+        continue
+if extension in ['.txt', '.pdf', '.doc', '.docx']:
+        path1 = from_dir + '/' + file_name                      
+        path2 = to_dir + '/' + "Document_Files"                         
+        path3 = to_dir + '/' + "Document_Files" + '/' + file_name
+
+if os.path.exists(path2):
+            print("Moving"+file_name+".....")
+
+
+            shutil.move(path1,path3)
+else:
+            os.mkdir(path2)
+            print("Moving"+file_name+"....")
+            shutil.move(path1,path3) 
